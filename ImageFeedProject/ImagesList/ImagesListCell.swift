c0606d8150a680ecad1ba49231f17dc274fb235b
Igorysssha <igorysssha@mail.ru>
2023-11-21 16:10:32 +0400

    sprint_8

--- ImageFeedProject/ImagesList/ImagesListCell.swift
+++ ImageFeedProject/ImagesList/ImagesListCell.swift
@@ -5,4 +5,11 @@
 //  Created by  Игорь Килеев on 25.10.2023.
 //
 
-import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+      static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+}
