4598dcc62ca68261393e76830e1507713905ba52
Igorysssha <igorysssha@mail.ru>
2024-01-24 20:08:02 +0400

    Проверил все по пунктам, от 1 до 9. В прошлый раз с 9 спринтом я сделал кучу ошибок, и постоянно создавал новые пулл реквесты, надеясь что я все исправлю в новом, в итоге в SourceTree у меня в ветках получилась какая то запутанная паутина, я пробовал мерджануть старые ветки до новых, пробовал удалить ненужное, по итогу все сломал. Потом пересмотрел кучу видео, и решил что самое простое это создать новый git проект, и в него влить последнюю удачную(работающую) ветку, вроде как все работает, и я даже стал лучше ориентироваться в SourceTree, но есть сомнения, что могут возникнуть проблемы у вас, у меня проект запускается, работает

---
+++ ImageFeedProject/ServiceAccessLayer/OAuth2/OAuth2Service.swift
@@ -0,0 +1,132 @@
+//
+//  OAuth2Service.swift
+//  ImageFeedProject
+//
+//  Created by  Игорь Килеев on 28.12.2023.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case httpStatusCode(Int)
+    case urlRequestError(Error)
+    case urlSessionError
+    
+}
+
+final class OAuth2Service {
+    static let shared = OAuth2Service()
+    
+    private let urlSession = URLSession.shared
+     
+    private (set) var authToken: String? {
+            get {
+                return OAuth2TokenStorage().token
+            }
+            set {
+                OAuth2TokenStorage().token = newValue
+    } }
+    
+    func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        
+        var components =  URLComponents(string: "https://unsplash.com/oauth/token")
+        components?.queryItems = [
+        URLQueryItem(name: "client_id", value: AccessKey),
+        URLQueryItem(name: "client_secret", value: SecretKey),
+        URLQueryItem(name: "redirect_uri", value: RedirectURI),
+        URLQueryItem(name: "code", value: code),
+        URLQueryItem(name: "grant_type", value: "authorization_code")
+        ]
+        
+        if let url = components?.url {
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            
+            let task = object(for: request) { [weak self] result in
+                        guard let self = self else { return }
+                        switch result {
+                        case .success(let body):
+                            let authToken = body.accessToken
+                            self.authToken = authToken
+                            completion(.success(authToken))
+                        case .failure(let error):
+                            completion(.failure(error))
+            } }
+                    task.resume()
+        }
+    }
+    
+    private struct OAuthTokenResponseBody: Codable {
+        let accessToken: String
+        let tokenType: String
+        let scope: String
+        let createAt: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case accessToken = "access_token"
+            case tokenType = "token_type"
+            case scope
+            case createAt = "created_at"
+        }
+    }
+}
+
+// MARK: - HTTP Request
+
+extension URLRequest {
+    static func makeHTTPRequest(
+        path: String,
+        httpMethod: String,
+        baseURL: URL = DefaultBaseURL
+    ) -> URLRequest {
+        var request = URLRequest(url: URL(string: path, relativeTo: baseURL)!)
+        request.httpMethod = httpMethod
+        return request
+    }
+}
+
+extension URLSession {
+    func data(
+        for request: URLRequest,
+        completion: @escaping (Result<Data, Error>) -> Void
+    ) -> URLSessionTask {
+        let fulfillCompletion: (Result<Data, Error>) -> Void = { result in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+}
+        let task = dataTask(with: request, completionHandler: { data, response, error in
+            if let data = data,
+                let response = response,
+                let statusCode = (response as? HTTPURLResponse)?.statusCode
+            {
+                if 200 ..< 300 ~= statusCode {
+                    fulfillCompletion(.success(data))
+                } else {
+                    fulfillCompletion(.failure(NetworkError.httpStatusCode(statusCode)))
+                }
+            } else if let error = error {
+                fulfillCompletion(.failure(NetworkError.urlRequestError(error)))
+            } else {
+                fulfillCompletion(.failure(NetworkError.urlSessionError))
+            }
+        })
+        task.resume()
+        return task
+        
+} }
+
+extension OAuth2Service {
+    private func object(
+        for request: URLRequest,
+        completion: @escaping (Result<OAuthTokenResponseBody, Error>) -> Void
+    ) -> URLSessionTask {
+        let decoder = JSONDecoder()
+        return urlSession.data(for: request) { (result: Result<Data, Error>) in
+            let response = result.flatMap { data -> Result<OAuthTokenResponseBody, Error> in
+                Result { try decoder.decode(OAuthTokenResponseBody.self, from: data) }
+            }
+            completion(response)
+        }
+    }
+}
